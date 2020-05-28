4f4c65c89ccbdca295db6dd48c5d02e2915fabc9
Fujimoto <genfujimoto@gmail.comi>
2020-05-28 11:29:07 +0900

    test

---
+++ 第5回課題.py
@@ -0,0 +1,9 @@
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import os
