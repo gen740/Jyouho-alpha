d674945eceac125527f6ad2e8284ddf0de62567c
Fujimoto <genfujimoto@gmail.comi>
2020-05-28 11:36:29 +0900

    test

--- 第5回課題.py
+++ 第5回課題.py
@@ -6,4 +6,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-import os
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+
+datagen = ImageDataGenerator()
