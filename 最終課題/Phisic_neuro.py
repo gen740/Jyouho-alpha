b34bc3662893b92e2797748bebe80bb061661c15
Fujimoto <genfujimoto@gmail.comi>
2020-06-05 16:37:22 +0900

    Merge branch 'master' of https://github.com/gen740/Jyouho-alpha
    # Conflicts:
    #	第五回課題.ipynb

--- 最終課題/Phisic_neuro.py
+++ 最終課題/Phisic_neuro.py
@@ -9,17 +9,23 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
+x_test, y_test = np.load("/Users/fujimotogen/Documents/GitHub/Jyouho-alpha/最終課題/x_test.npy"),\
+                 np.load("/Users/fujimotogen/Documents/GitHub/Jyouho-alpha/最終課題/y_test.npy")
 
-inputs = Input((16))
-x = Dense(1200, activation='relu')(x)
-x = Dropout(0.4)(x)
-x = Dense(1200, activation='relu')(x)
-x = Dropout(0.4)(x)
+inputs = Input((16,))
+x = Dense(300, activation='sigmoid')(inputs)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
+x = Dense(300, activation='sigmoid')(x)
 output = Dense(12, activation='softmax')(x)
 
 model = keras.Model(inputs, output)
 model.compile(
-    loss='categorical_crossentropy',
+    loss='mean_squared_error',
     optimizer=keras.optimizers.Adam(),
     metrics=["accuracy"],
 )
@@ -28,8 +34,8 @@ keras.utils.plot_model(model,
                        show_shapes=True,
                        show_layer_names=True)
 
-epochs = 100
-history = model.fit(np.array([[random_default()] for i in range(100)]), np.array([[calc_TBP(random_default())] for i in range(100)]),batch_size=5 ,epochs=epochs, verbose=1)
+epochs = 10
+history = model.fit(x_test, y_test, batch_size=1 ,epochs=epochs, verbose=1)
 
 test_scores = model.evaluate(x_test, y_test, verbose=2)
 print("テスト損失:", test_scores[0])
