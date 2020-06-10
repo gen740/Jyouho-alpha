5c8398f945e0d0f1cfdf8ff1fc67431b0eaaaa0b
Fujimoto <genfujimoto@gmail.comi>
2020-06-10 17:50:19 +0900

    Merge branch 'master' of https://github.com/gen740/Jyouho-alpha
    # Conflicts:
    #	第五回課題.ipynb

--- TB_problem.py
+++ TB_problem.py
@@ -5,7 +5,7 @@ G = 1  # 6.674 * 10 ** (-11) todo
 
 
 class body:
-    def __init__(self, default_position,  default_vel, mass):
+    def __init__(self, default_position, default_vel, mass):
         self.velocity = np.array([default_vel])
         self.position = np.array([default_position])
         self.mass = mass
@@ -25,19 +25,17 @@ class TBP:
         self.body2 = body2
         self.body3 = body3
 
-
     def calc_a(self):
         self.r = np.array([self.body1.pos() - self.body2.pos(),
-                          self.body2.pos() - self.body3.pos(),
-                          self.body3.pos() - self.body1.pos()])
-
+                           self.body2.pos() - self.body3.pos(),
+                           self.body3.pos() - self.body1.pos()])
 
         self.d = np.array(np.linalg.norm([self.r[0], self.r[1], self.r[2]], axis=1))
         self.abs_r = self.r / np.array([[self.d[i], self.d[i]] for i in range(3)])
 
-        self.abs_F = -G*np.array([self.mass[0]*self.mass[1], self.mass[1]*self.mass[2], self.mass[2]*self.mass[0]])\
-                        /self.d
-
+        self.abs_F = -G * np.array(
+            [self.mass[0] * self.mass[1], self.mass[1] * self.mass[2], self.mass[2] * self.mass[0]]) \
+                     / self.d
 
         self.F_vec = np.array([[self.abs_F[i], self.abs_F[i]] for i in range(3)]) * self.abs_r
         self.F = self.F_vec - np.roll(self.F_vec, 2)
@@ -53,8 +51,8 @@ class TBP:
             i.velocity = np.append(i.velocity, np.array([i.vel() + self.a[self.bodies.index(i)] * self.dt]), axis=0)
 
     def t_(self, t):
-        t = t*10
-        repeat = int(t/self.dt)
+        t = t * 10
+        repeat = int(t / self.dt)
         for i in range(repeat):
             self.dx()
 
@@ -73,7 +71,7 @@ counter = 0
 def calc_TBP(arg):
     global counter
     counter = counter + 1
-    #print(arg[12])
+    # print(arg[12])
     body1 = body(arg[0:2], arg[6:8], arg[12])
     body2 = body(arg[2:4], arg[8:10], arg[13])
     body3 = body(arg[4:6], arg[10:12], arg[14])
@@ -85,13 +83,9 @@ def calc_TBP(arg):
 
 
 def random_default():
-    return np.append((np.random.random(size=(15))-0.5)*2, 5)
+    return np.append((np.random.random(size=(15)) - 0.5) * 2, 5)
 
 
 if "__main__" == __name__:
-
-
     print(random_default())
     print(calc_TBP(random_default()))
-
-
