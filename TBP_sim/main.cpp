02d64f71d956be14bc32896436c0520a430855d1
Fujimoto <genfujimoto@gmail.comi>
2020-07-11 17:10:23 +0900

    ランダムを実装したい

--- TBP_sim/main.cpp
+++ TBP_sim/main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 
 // Other Functions
-void Show_data(star stars[]);
 namespace fs = std::filesystem;
 
 int main()
@@ -17,8 +16,8 @@ int main()
         {1, 0.7, 0.1, 0.0, 0.0, 0.0, 0.0},
     };
 
-    Show_data(stars);
     TBP test(stars);
+    test.Show();
     std::ofstream("data/options.txt");
     file.open("data/options.txt");
     file << "{ 'NUMBER_OF_STAR': " << NUMBER_OF_STAR << std::endl
@@ -34,25 +33,4 @@ int main()
     }
     test.Show();
     return 0;
-}
-
-
-void Show_data(star stars[])
-{
-    for (int i = 0; i < NUMBER_OF_STAR; i++) {
-        std::cout << std::fixed;
-        std::cout << std::setprecision(8)
-                  << "Star number = " << i + 1 << std::endl
-                  << "mass    : " << stars[i].m << std::endl
-                  << "position: "
-                  << stars[i].x[0] << " "
-                  << stars[i].x[1] << " "
-                  << stars[i].x[2] << " \n"
-                  << "momentum: "
-                  << stars[i].p[0] << " "
-                  << stars[i].p[1] << " "
-                  << stars[i].p[2] << " \n\n";
-    }
-    std::cout << "---------------------------------------------------------"
-              << "\n";
 }
